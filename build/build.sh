d0f4b33e71ee7f59da6d613959682985a3198dbf
Keisuke Kobayashi <keisuke.kobayashi@gree.net>
2013-02-10 22:40:30 +0900

    Fixed build script

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,14 @@ rm ../Loop/gen/com/kskkbys/loop/BuildConfig.java
 rm ../Loop/gen/com/kskkbys/loop/R.java
 
 # Build with ant
-cd ../Loop
+cd ../facebook-android-sdk-3.0/facebook
+android update project --path ./
+
+cd ../../ActionBarSherlock/library
+android update project --path ./
+
+cd ../../Loop
+android update project --path ./
 ant clean
 ant release
 
