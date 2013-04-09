29a5cab7b8d70dbb3b56bbd842b7c0fe5d13a58d
Keisuke Kobayashi <keisuke.kobayashi@gree.net>
2013-04-09 19:40:36 +0200

    Removed unused import

--- Loop/src/com/kskkbys/loop/notification/NotificationManager.java
+++ Loop/src/com/kskkbys/loop/notification/NotificationManager.java
@@ -4,7 +4,6 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 
