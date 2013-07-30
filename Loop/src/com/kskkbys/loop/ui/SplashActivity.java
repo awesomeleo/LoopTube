eab830be45de546944b93bd91377160606a91e3e
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2013-07-30 23:15:31 +0900

    Show splash at least 2 seconds

--- Loop/src/com/kskkbys/loop/ui/SplashActivity.java
+++ Loop/src/com/kskkbys/loop/ui/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.kskkbys.loop.ui;
 
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.kskkbys.loop.LoopApplication;
 import com.kskkbys.loop.R;
@@ -13,8 +14,6 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.sax.StartElementListener;
 import android.widget.TextView;
 
 /**
@@ -26,61 +25,79 @@ import android.widget.TextView;
 public class SplashActivity extends Activity {
 
 	private static final String TAG = SplashActivity.class.getSimpleName();
-	
+
+	// 3 seconds
+	private static final long MIN_SPLASH_INTERVAL = 2000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		KLog.v(TAG, "onCreate");
-		
+
 		// If first launch, show splash
 		LoopApplication app = (LoopApplication)getApplication();
 		if (app.isFirstLaunch()) {
 			FlurryLogger.logEvent(FlurryLogger.SEE_SPLASH);
 			setContentView(R.layout.activity_splash);
-			
+
 			// Custom font
 			TextView title = (TextView)findViewById(R.id.splash_title);
 			title.setTypeface(Typeface.createFromAsset(getAssets(), "GeosansLight.ttf"));
-			
+
 			TextView desc = (TextView)findViewById(R.id.splash_description);
 			desc.setTypeface(Typeface.createFromAsset(getAssets(), "GeosansLight-Oblique.ttf"));
-			
+
 			// Start to restore history.
 			restoreHistory();
 		} else {
 			startMainActivity();
 		}
 	}
-	
+
 	private void startMainActivity() {
 		startActivity(new Intent(SplashActivity.this, MainActivity.class));
 		finish();
 	}
-	
+
 	private void restoreHistory() {
 		LoopApplication app = (LoopApplication)getApplication();
 		ArtistStorage storage = app.getArtistStorage();
 		LoadHistoryTask task = new LoadHistoryTask();
 		task.execute(storage);
 	}
-	
+
 	/**
 	 * 
 	 * @author Keisuke Kobayashi
 	 *
 	 */
 	private class LoadHistoryTask extends AsyncTask<ArtistStorage, Integer, Boolean> {
-		
+
+		private long mStartTime;
+
+		@Override
+		protected void onPreExecute() {
+			mStartTime = System.currentTimeMillis();
+		}
+
 		@Override
 		protected Boolean doInBackground(ArtistStorage... params) {
 			ArtistStorage storage = params[0];
 			storage.restore();
 			return true;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Boolean result) {
-			startMainActivity();
+			long endTime = System.currentTimeMillis();
+			long spent = endTime - mStartTime;
+			KLog.v(TAG, "Loading history takes " + spent + " milliseconds.");
+			new Timer().schedule(new TimerTask() {
+				@Override
+				public void run() {
+					startMainActivity();
+				}
+			}, Math.max(0L, MIN_SPLASH_INTERVAL - spent));
 		}
 	}
 }
